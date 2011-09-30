2d5eb955a755900e80f2ce2b5427ac7a0850ee1e
Nick Williams <mr.nicksta@gmail.com>
2011-09-30 02:40:08 +0100

    first commit, fingers crossed :-)

---
+++ ReadyBinder.js
@@ -0,0 +1,47 @@
+(function ($) {
+
+    var defaults =  {
+        target : $("body"),
+        attribute : "class",
+        delimiter : " ",
+        handlers : {}
+    };
+
+    function wireUp(options) {
+
+        var settings, handlerKeys;
+
+        settings = $.extend({}, defaults, options);
+        handlerKeys = settings.target
+                        .attr(settings.attribute)
+                        .split(settings.delimiter);
+
+        for (var i = 0; i < handlerKeys.length; i++) {
+
+            var handler = settings.handlers[handlerKeys[i]];
+
+            if (handler && typeof handler === "function") {
+                handler();
+            }
+
+        }
+
+    }
+
+    $.start = function(options) {
+
+        //allow for selector as well as wrapped jQuery set
+        if(typeof options.target === "string") {
+            options.target = $(options.target);
+        }
+
+        wireUp(options);
+    };
+
+    $.fn.start = function(options) {
+        wireUp(options);
+    }
+
+    $.fn.start.defaults = defaults;
+
+})(jQuery);
